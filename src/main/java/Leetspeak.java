6144efa562f2b30dffba73e1acd30fee771e1f03
10igeorge <10izzygeorge@gmail.com>
2016-02-09 06:40:12 -0800

    add velocity and spark files

--- src/main/java/Leetspeak.java
+++ src/main/java/Leetspeak.java
@@ -1,11 +1,34 @@
+import java.util.Map;
+import java.util.HashMap;
+
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
 public class Leetspeak {
-  public static void main(String[] args){}
+  public static void main(String[] args){
+    String layout = "templates/layout.vtl";
 
-    public String isNotLeetspeak(String word){
-      return word;
-    }
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/home.vtl");
 
-    public String isLeetspeak(String word) {
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/results", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/results.vtl");
+
+      String text = request.queryParams();
+
+      model.put("text", text);
+
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+  }
+
+    public static String isLeetspeak(String word) {
     word = word.toLowerCase();
     for (Integer i=0; i <= word.length(); i++){
       word = word.replaceAll("o", "0");
--- src/test/java/LeetspeakTest.java
+++ src/test/java/LeetspeakTest.java
@@ -1,47 +1,35 @@
 import org.junit.*;
 import static org.junit.Assert.*;
 
-public class LeetspeakTest{
-
-  @Test
-  public void isNotLeetspeak_returnsSameStringIfNoLeetspeakIsPresent_wordDoesntChange(){
-    Leetspeak LeetspeakTest = new Leetspeak();
-    assertEquals("happy", LeetspeakTest.isNotLeetspeak("happy"));
-  }
+public class Leetspeak{
 
   @Test
   public void isLeetspeak_returnCorrectRegardlessOfCase_wordChanges () {
-    Leetspeak LeetspeakTest = new Leetspeak();
-    assertEquals("1 scr3am", LeetspeakTest.isLeetspeak("I scream"));
+    assertEquals("1 scr3am", Leetspeak.isLeetspeak("I scream"));
   }
 
   @Test
   public void isLeetspeak_returns3ifletterisE_wordChanges() {
-    Leetspeak LeetspeakTest = new Leetspeak();
-    assertEquals("3l3phant", LeetspeakTest.isLeetspeak("elephant"));
+    assertEquals("3l3phant", Leetspeak.isLeetspeak("elephant"));
   }
 
   @Test
   public void isLeetspeak_returns0ifletterisO_wordChanges() {
-    Leetspeak LeetspeakTest = new Leetspeak();
-    assertEquals("b00 b00", LeetspeakTest.isLeetspeak("boo boo"));
+    assertEquals("b00 b00", Leetspeak.isLeetspeak("boo boo"));
   }
 
   @Test
   public void isLeetspeak_returns1ifletterisI_wordChanges() {
-    Leetspeak LeetspeakTest = new Leetspeak();
-    assertEquals("1 l1k3 1k3", LeetspeakTest.isLeetspeak("i like ike"));
+    assertEquals("1 l1k3 1k3", Leetspeak.isLeetspeak("i like ike"));
   }
 
   @Test
   public void isLeetspeak_returnsZifletterisS_wordChanges() {
-    Leetspeak LeetspeakTest = new Leetspeak();
-    assertEquals("r0z3z ar3 r3d", LeetspeakTest.isLeetspeak("roses are red"));
+    assertEquals("r0z3z ar3 r3d", Leetspeak.isLeetspeak("roses are red"));
   }
 
   @Test
   public void isLeetspeak_doesNotReplaceSWhenFirstLetter_wordChanges() {
-    Leetspeak LeetspeakTest = new Leetspeak();
-    assertEquals("sazzy sunzh1n3", LeetspeakTest.isLeetspeak("sassy sunshine"));
+    assertEquals("sazzy sunzh1n3", Leetspeak.isLeetspeak("sassy sunshine"));
   }
 }
